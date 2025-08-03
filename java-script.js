0046ca843d359d56c08a74e4bdc4a41afa895474
Daniel Ciupek <danter005@o2.pl>
2025-08-03 23:22:59 +0200

    new lesson wtf

--- java-script.js
+++ java-script.js
@@ -1,7 +1,7 @@
-const firstName = 'Daniel' ;
-const age = 34 ;
+const firstName = "Daniel";
+const age = 34;
 
-alert ('WTF');
-console.log (firstName);
-console.log (age);
-console.log (`Siema, nazywam się ${firstName} i mam ${age} lata`)
+
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lata`);
