8b45537aca49edb88da1346951bf5fa52d599ea0
Daniel Ciupek <danter005@o2.pl>
2025-08-10 15:37:10 +0200

    normalize-table-other

--- java-script.js
+++ java-script.js
@@ -6,3 +6,54 @@ console.log(firstName);
 console.log(age);
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lata`);
 
+
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    isLightOn: true,
+    commander: {
+        name: 'Darth Vader',
+        age: 44
+    }
+}
+console.log(deathStar.name)
+
+
+
+
+
+console.log(console)
+console.log(deathStar.commander)
+deathStar.fire ('Rebel Ship')
+
+
+const humanOne = {
+    name: 'Daniel',
+    age: 34,
+    address: {
+        street: 'Warszawska',
+        city: 'Białystok',
+    }
+
+
+}
+
+const humanTwo = {
+    name: 'DaZenek',
+    age: 34,
+    address: humanOne.address.city
+    
+
+}
+
+
+humanTwo.age = 40;
+console.log (humanTwo)
+console.log (humanOne)
